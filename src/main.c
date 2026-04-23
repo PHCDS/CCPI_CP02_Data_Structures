3adeebd2b0aba7fc8e1b644bfe859a19c6549197
PHCDS <pedroc.santos2018@gmail.com>
2026-04-23 10:46:26 -0300

    adicionado void classificação

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
+void classificacao(float m) {
+    if (m >= 6 && m <= 10) {
+        printf("Situacao: APROVADO\n");
+    } else if (m >= 4 && m < 6) {
+        printf("Situacao: RECUPERACAO\n");
+    } else {
+        printf("Situacao: REPROVADO\n");
+    }
+}
+
 int main() {
     
 
