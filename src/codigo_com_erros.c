bb80e4ce324bf872eaf95c45afefca80156ae6c7
victorcbinot <victorcbinot@gmail.com>
2026-04-23 11:13:49 -0300

    O código incorreto foi arrumado

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,17 +1,17 @@
-#include "stdio.h"
+#include<stdio.h> //Foi adicionado o <> e as aspas foram removidas
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media; //Foi adicionado o ";"
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2); //Foi adicionado o "&"
 
-    media = nota1 + nota2 / 2;
+    media = (nota1 + nota2) / 2; //Parênteses adicionado
 
-    if (media = 6) {
+    if (media >= 6) { // Foi adicionado o >, pois a lógica estava errada 
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
