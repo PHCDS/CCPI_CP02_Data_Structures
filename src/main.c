93afbc162089658afb5a860736316b23600e22b0
PHCDS <pedroc.santos2018@gmail.com>
2026-04-23 10:49:13 -0300

    adicionado media aritimetica

--- src/main.c
+++ src/main.c
@@ -2,12 +2,16 @@
 
 void classificacao(float m) {
     if (m >= 6 && m <= 10) {
-        printf("Situacao: APROVADO\n");
+        printf("aprovado\n");
     } else if (m >= 4 && m < 6) {
-        printf("Situacao: RECUPERACAO\n");
+        printf("recuperacao\n");
     } else {
-        printf("Situacao: REPROVADO\n");
+        printf("reprovado\n");
     }
+}
+
+float media_aritmetica(float nota1, float nota2) {
+    return (nota1 + nota2) / 2;
 }
 
 int main() {
